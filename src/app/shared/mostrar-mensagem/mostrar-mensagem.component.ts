6a3bfdecb74baa677d4174ffecba8d5b747ead25
Maria Luiza Moura <malumoura10@gmail.com>
2022-04-28 16:42:24 -0300

    Criação do modulo shared e componente monstrar mensagem

---
+++ src/app/shared/mostrar-mensagem/mostrar-mensagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-mensagem',
+  templateUrl: './mostrar-mensagem.component.html',
+  styleUrls: ['./mostrar-mensagem.component.scss']
+})
+export class MostrarMensagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MostrarMensagemComponent } from './mostrar-mensagem/mostrar-mensagem.component';
+
+
+
+@NgModule({
+  declarations: [
+    MostrarMensagemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
